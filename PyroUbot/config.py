2489f999ed8ccba6e52ea6aef1ec91a61cb430db
bsLbskqp018bslw <stok.pancingan777@gmail.com>
2024-12-31 20:46:46 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,15 +5,15 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "60"))
 
-DEVS = list(map(int, os.getenv("DEVS", "5736271266").split()))
+DEVS = list(map(int, os.getenv("DEVS", "6897960987").split()))
 
 API_ID = int(os.getenv("API_ID", "29119489"))
 
 API_HASH = os.getenv("API_HASH", "85dde49bb0c55b525d4f396d06209dd8")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8146429595:AAFlHTHRYAlhdE-41ZTkp12Ypc_FmmBOB3A")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8115675477:AAH37y0bEEVodAvqOvllC8LvWFj1M9tS9r8")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "5736271266"))
+OWNER_ID = int(os.getenv("OWNER_ID", "6897960987"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285").split()))
 
